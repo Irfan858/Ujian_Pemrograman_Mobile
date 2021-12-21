ba867e4d1e8a6772195a969a57542d599c3c238d
Irfan Kurniawan <irfankurniawan858@gmail.com>
2021-12-21 22:08:42 +0700

    Membuat File Ujian

---
+++ app/src/main/java/com/iktech/ujianpemrogramanmobile/MainActivity.java
@@ -0,0 +1,27 @@
+package com.iktech.ujianpemrogramanmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    //Soal No 5 Bonus
+    //Silahkan Kerjakan Pada File Berikut ini
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        button = findViewById(R.id.?);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//               Isikan Sisanya Disini
+//            }
+//        });
+    }
+}
